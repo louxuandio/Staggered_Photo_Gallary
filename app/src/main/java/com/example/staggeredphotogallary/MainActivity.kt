e42eaa024f2d9f38b053be945c6e2560bf006e25
Lou Xuan Dio <louxuan@bu.edu>
2025-02-10 02:28:50 -0500

    figure out how to realize xml and parse, add TODOs

--- app/src/main/java/com/example/staggeredphotogallary/MainActivity.kt
+++ app/src/main/java/com/example/staggeredphotogallary/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.staggeredphotogallary.ui.theme.StaggeredPhotoGallaryTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,7 +37,31 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+//TODO: set up photos.xml
+//TODO: use XmlPullParser to parse into drawable
+//TODO: load photo
 @Composable
 fun MainScreen(){
-    
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(8.dp) //ChatGPT
+    ) {
+        /*
+        items(photos){photo ->
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(photo.second.dp)
+            ){
+                Image(
+                    painter = rememberImagePainter(photo.first),
+                    contentDescription = null, //TODO
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+        }
+
+         */
+    }
 }
