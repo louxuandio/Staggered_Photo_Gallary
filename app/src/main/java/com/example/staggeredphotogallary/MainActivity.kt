6496fe3de22b5f38f4f84ba83235ff99f0210744
Lou Xuan Dio <louxuan@bu.edu>
2025-02-10 20:41:24 -0500

    add the enlargement function

--- app/src/main/java/com/example/staggeredphotogallary/MainActivity.kt
+++ app/src/main/java/com/example/staggeredphotogallary/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -26,11 +28,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -59,12 +64,13 @@ fun MainScreen(){
     var context = LocalContext.current
     val photos = loadPhotos(context)
     //val coroutineScope = rememberCoroutineScope()
-
+    val selectedImage = remember { mutableStateOf<Int>(-1) }
     LazyVerticalGrid(
         columns = GridCells.Fixed(2),
         modifier = Modifier.fillMaxSize(),
         contentPadding = PaddingValues(8.dp), //ChatGPT
         verticalArrangement = Arrangement.spacedBy(0.dp)
+
     ) {
         items(photos){ photo ->
             val (resId, title) = photo
@@ -72,15 +78,36 @@ fun MainScreen(){
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(300.dp)
+                    .clickable {
+                        selectedImage.value = if (selectedImage.value == resId) -1 else resId
+                    }
             ){
+                val targetSize by animateDpAsState(targetValue = if (selectedImage.value == resId) 400.dp else 200.dp) //ChatGPT
                 Image(
                     painter = painterResource(id = resId),
                     contentDescription = title, //TODO
                     modifier = Modifier.fillMaxSize()
-                        .width(200.dp)
+                        .width(targetSize)
                         .height((100..300).random().dp) //ChatGPT
                 )
             }
+        }
+    }
+    if (selectedImage.value != -1){
+        val resId = selectedImage.value
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = Color.Gray)
+        ){
+            Image(
+                painter = painterResource(id = resId),
+                contentDescription = "selected",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .clickable { selectedImage.value = -1 }
+                    .padding(32.dp)
+            )
         }
     }
 }
